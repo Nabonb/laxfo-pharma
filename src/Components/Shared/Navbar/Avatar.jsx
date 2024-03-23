2bbc71796c526b258e51c5b8c284e6e5229f844b
Pratham Barua Nabo <nabobarua2@gmail.com>
2024-03-23 13:17:40 +0600

    registration added

--- src/Components/Shared/Navbar/Avatar.jsx
+++ src/Components/Shared/Navbar/Avatar.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import avatarImg from '../../../assets/images/placeholder.jpg'
-import { useContext } from 'react';
-import { AuthContext } from '../../../providers/AuthProvider';
+import React from "react";
+import avatarImg from "../../../assets/images/placeholder.jpg";
+import { useContext } from "react";
+import { AuthContext } from "../../../providers/AuthProvider";
 const Avatar = () => {
-    const {user} = useContext(AuthContext)
-    return (
-        <div>
-            <img className='rounded-full w-8' src={user && user.photoURL ? user.photoURL : avatarImg} alt="" />
-        </div>
-    );
+  const { user } = useContext(AuthContext);
+  return (
+      <img
+        className="rounded-full"
+        height="30"
+        width="30"
+        src={user && user.photoURL ? user.photoURL : avatarImg}
+        alt="profile"
+      />
+  );
 };
 
 export default Avatar;
